cb199aef31947245b8462c35cb889631648603bd
gubatron <gubatron@gmail.com>
2014-12-08 17:07:36 -0500

    revert  keyword removal.

--- core/com/frostwire/search/CrawlRegexSearchPerformer.java
+++ core/com/frostwire/search/CrawlRegexSearchPerformer.java
@@ -38,7 +38,7 @@ public abstract class CrawlRegexSearchPerformer<T extends CrawlableSearchResult>
     }
 
     @Override
-    protected List<? extends SearchResult> searchPage(String page) {
+    protected final List<? extends SearchResult> searchPage(String page) {
         return PerformersHelper.searchPageHelper(this, page, regexMaxResults);
     }
 }
