4ba4fe332f9d39404935f86b63cac99b34dc54ec
gubatron <gubatron@gmail.com>
2015-02-18 10:35:06 -0500

    renaming package due to typo - https://github.com/frostwire/frostwire-desktop/issues/419

--- core/com/frostwire/search/tpb/TPBSearchResult.java
+++ core/com/frostwire/search/tpb/TPBSearchResult.java
@@ -16,7 +16,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package com.frostwire.search.tbp;
+package com.frostwire.search.tpb;
 
 import java.util.Calendar;
 import java.util.HashMap;
