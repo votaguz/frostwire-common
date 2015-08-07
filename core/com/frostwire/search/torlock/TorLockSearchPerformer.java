37ef14f3b315c231e01981640b056b2c239b0227
gubatron <gubatron@gmail.com>
2015-08-07 16:58:09 -0400

    Fixes TorLock search.

--- core/com/frostwire/search/torlock/TorLockSearchPerformer.java
+++ core/com/frostwire/search/torlock/TorLockSearchPerformer.java
@@ -30,7 +30,7 @@ public class TorLockSearchPerformer extends TorrentRegexSearchPerformer<TorLockS
 
     private static final int MAX_RESULTS = 10;
     private static final String REGEX = "(?is)<a href=/torrent/([0-9]*?/.*?\\.html)>";
-    private static final String HTML_REGEX = "(?is).*?<td><b>Name:</b></td><td>(?<filename>.*?).torrent</td>.*?<td><b>Size:</b></td><td>(?<filesize>.*?) in .*? file.*?</td>.*?<td><b>Added:</b></td><td>Uploaded on (?<time>.*?) by .*?color:#FF5400.*?>(?<seeds>\\d*?)</b>.*? seeders.*?<a href=\"/tor/(?<torrentid>.*?).torrent\"><img.*?";
+    private static final String HTML_REGEX = "(?is).*?<a href=\\\"/tor/(?<torrentid>.*?).torrent\\\"><img.*?<td><b>Name:</b></td><td>(?<filename>.*?).torrent</td></tr>.*?<b>Size:</b></td><td>(?<filesize>.*?) in .*? file.*?</td>.*?<td><b>Added:</b></td><td>Uploaded on (?<time>.*?) by .*?color:#FF5400.*?>(?<seeds>\\d*?)</b>.*? seeders.*?";
 
     public TorLockSearchPerformer(String domainName, long token, String keywords, int timeout) {
         super(domainName, token, keywords, timeout, 1, 2 * MAX_RESULTS, MAX_RESULTS, REGEX, HTML_REGEX);
@@ -53,28 +53,31 @@ public class TorLockSearchPerformer extends TorrentRegexSearchPerformer<TorLockS
         return new TorLockSearchResult(getDomainName(), sr.getDetailsUrl(), matcher);
     }
 
+
     /**
      public static void main(String[] args) throws Exception {
-     //REGEX TEST CODE
-     String resultsHTML = FileUtils.readFileToString(new File("/Users/gubatron/Desktop/torlock-results.html"));
-     final Pattern resultsPattern = Pattern.compile(REGEX);
+         //REGEX TEST CODE
+         //String resultsHTML = FileUtils.readFileToString(new File("/Users/gubatron/Desktop/torlock-results.html"));
+         //final Pattern resultsPattern = Pattern.compile(REGEX);
 
-     final SearchMatcher matcher = SearchMatcher.from(resultsPattern.matcher(resultsHTML));
-     while (matcher.find()) {
-     //System.out.println(matcher.group(1));
-     }
-     String resultHTML = FileUtils.readFileToString(new File("/Users/gubatron/Desktop/torlock-result.html"));
-     final Pattern detailPattern = Pattern.compile(HTML_REGEX);
-     final SearchMatcher detailMatcher = SearchMatcher.from(detailPattern.matcher(new MaxIterCharSequence(resultHTML,resultHTML.length())));
+         //final SearchMatcher matcher = SearchMatcher.from(resultsPattern.matcher(resultsHTML));
+         //while (matcher.find()) {
+         //System.out.println(matcher.group(1));
+         //}
 
-     if (detailMatcher.find()) {
-     System.out.println("File name: " + detailMatcher.group("filename"));
-     System.out.println("Size: " + detailMatcher.group("filesize"));
-     System.out.println("Date: " + detailMatcher.group("time"));
-     System.out.println("Seeds: " + detailMatcher.group("seeds"));
-     System.out.println("TorrentID: " + detailMatcher.group("torrentid"));
-     } else {
-     System.out.println("No detail matched.");
+         String resultHTML = FileUtils.readFileToString(new File("/Users/gubatron/Desktop/torlock-result.html"));
+         final Pattern detailPattern = Pattern.compile(HTML_REGEX);
+         final SearchMatcher detailMatcher = SearchMatcher.from(detailPattern.matcher(new MaxIterCharSequence(resultHTML,4*resultHTML.length())));
+
+         if (detailMatcher.find()) {
+             System.out.println("TorrentID: " + detailMatcher.group("torrentid"));
+             System.out.println("File name: " + detailMatcher.group("filename"));
+             System.out.println("Size: " + detailMatcher.group("filesize"));
+             System.out.println("Date: " + detailMatcher.group("time"));
+             System.out.println("Seeds: " + detailMatcher.group("seeds"));
+         } else {
+             System.out.println("No detail matched.");
+         }
      }
-     }*/
+     */
 }
